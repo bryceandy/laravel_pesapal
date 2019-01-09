17e4d0d631e0436225083df0e2a747453cf3685f
bryceandy <bryceandy@rocketmail.com>
2019-01-09 19:33:40 +0300

    csrf

--- src/resources/views/details.blade.php
+++ src/resources/views/details.blade.php
@@ -5,7 +5,6 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <meta name="csrf-token" content="{{csrf_token()}}">
 
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/semantic-ui@2.4.2/dist/semantic.min.css">
 
@@ -14,7 +13,7 @@
 <body>
 
 <form action="/iframe" method="post">
-    {{csrf_field()}}
+    @csrf
 
     <div class="ui form">
         <div class="fields">
