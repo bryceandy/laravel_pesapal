bd753f09d6bbe684b0630fd8f8056189e07e410c
bryceandy <bryceandy@rocketmail.com>
2019-01-09 16:58:23 +0300

    service provider added

---
+++ src/PesapalServiceProvider.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Bryceandy\Laravel_Pesapal;
+
+
+use Illuminate\Support\ServiceProvider;
+
+class PesapalServiceProvider extends ServiceProvider
+{
+
+    public function boot()
+    {
+
+
+
+    }
+
+    public function register()
+    {
+
+        
+
+    }
+
+}
