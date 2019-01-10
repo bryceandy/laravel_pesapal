602ea63ff3a567c404edf9177eb8411b8723a496
bryceandy <bryceandy@rocketmail.com>
2019-01-10 06:05:49 +0300

    publish all

--- src/PesapalServiceProvider.php
+++ src/PesapalServiceProvider.php
@@ -18,23 +18,13 @@ class PesapalServiceProvider extends ServiceProvider
         $this->publishes([
             __DIR__.'/config/pesapal.php' => config_path('pesapal.php'),
             __DIR__.'/resources/views/' => resource_path('views/pesapal'),
+            __DIR__.'/Http/Controllers' => app_path('/Http/Controllers'),
+            __DIR__.'/Models' => app_path('/'),
+            __DIR__.'/database/migrations' => database_path('migrations'),
+            __DIR__.'/assets' => public_path('pesapal')
+
         ]);
 
-        $this->publishes([
-            __DIR__.'/assets' => public_path('pesapal'),
-        ], 'public');
-
-        $this->publishes([
-            __DIR__.'/database/migrations' => database_path('migrations')
-        ], 'migrations');
-
-        $this->publishes([
-            __DIR__.'/Models' => app_path('/')
-        ], 'models');
-
-        $this->publishes([
-            __DIR__.'/Http/Controllers' => app_path('/Http/Controllers')
-        ], 'controllers');
     }
 
     public function register()
