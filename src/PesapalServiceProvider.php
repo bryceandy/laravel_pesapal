c87ca5e435a11f56e069e2aad5ef9b5cdf41287c
bryceandy <bryceandy@rocketmail.com>
2020-06-13 15:14:29 +0300

    Differentiate booted services from those in the console with those that are auto-discovered

--- src/PesapalServiceProvider.php
+++ src/PesapalServiceProvider.php
@@ -8,10 +8,34 @@ class PesapalServiceProvider extends ServiceProvider
 {
     public function boot()
     {
+        if ($this->app->runningInConsole()) {
+
+            $this->registerPublishing();
+        }
+
+        $this->registerResources();
+    }
+
+    public function register()
+    {
+        $this->mergeConfigFrom(__DIR__.'/config/pesapal.php', 'laravel_pesapal');
+    }
+
+    /**
+     * Loads resources
+     */
+    private function registerResources()
+    {
         $this->loadRoutesFrom(__DIR__.'/routes/web.php');
         $this->loadViewsFrom(__DIR__.'/resources/views', 'laravel_pesapal');
         $this->loadMigrationsFrom(__DIR__ . '/database/migrations');
+    }
 
+    /**
+     * Publishes resources when commands are initiated from the command line
+     */
+    private function registerPublishing()
+    {
         $this->publishes([
             __DIR__.'/config/pesapal.php' => config_path('pesapal.php'),
             __DIR__.'/resources/views/' => resource_path('views/vendor/laravel_pesapal'),
@@ -19,12 +43,6 @@ class PesapalServiceProvider extends ServiceProvider
             __DIR__.'/Models' => app_path('/'),
             __DIR__.'/database/migrations' => database_path('migrations'),
             __DIR__.'/assets' => public_path('pesapal')
-
         ]);
-    }
-
-    public function register()
-    {
-        $this->mergeConfigFrom(__DIR__.'/config/pesapal.php', 'laravel_pesapal');
     }
 }
