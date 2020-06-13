7892959981fb4f151dbb6c39ba27b7d810ddab78
bryceandy <bryceandy@rocketmail.com>
2020-06-13 19:08:15 +0300

    Add access modifiers

--- src/Pesapal/OAuthConsumer.php
+++ src/Pesapal/OAuthConsumer.php
@@ -5,16 +5,18 @@ namespace Bryceandy\Laravel_Pesapal\Pesapal;
 class OAuthConsumer
 {
     public string $key;
+
     public string $secret;
+
     public string $callback_url;
 
-    function __construct($key, $secret, $callback_url = NULL) {
+    public function __construct($key, $secret, $callback_url = NULL) {
         $this->key = $key;
         $this->secret = $secret;
         $this->callback_url = $callback_url;
     }
 
-    function __toString() {
+    public function __toString() {
         return "OAuthConsumer[key=$this->key,secret=$this->secret]";
     }
 }
