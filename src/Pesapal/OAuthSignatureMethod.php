924283311a80d6a2b932a6aa887ac7c4d89d1173
bryceandy <bryceandy@rocketmail.com>
2020-06-13 21:07:32 +0300

    Refactor oauth signature method

--- src/Pesapal/OAuthSignatureMethod.php
+++ src/Pesapal/OAuthSignatureMethod.php
@@ -2,12 +2,19 @@
 
 namespace Bryceandy\Laravel_Pesapal\Pesapal;
 
-
 class OAuthSignatureMethod
 {
-    public function check_signature(&$request, $consumer, $token, $signature) {
+    /**
+     * @param $request
+     * @param $consumer
+     * @param $token
+     * @param $signature
+     * @return bool
+     */
+    public function check_signature(&$request, $consumer, $token, $signature)
+    {
         $built = $this->build_signature($request, $consumer, $token);
+
         return $built == $signature;
     }
-
 }
