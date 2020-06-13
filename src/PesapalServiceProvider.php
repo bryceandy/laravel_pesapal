3e592eab75c13dd282e863ff1f01cd05ee8b0beb
bryceandy <bryceandy@rocketmail.com>
2020-06-13 15:03:29 +0300

    Modify the location to publish views

--- src/PesapalServiceProvider.php
+++ src/PesapalServiceProvider.php
@@ -14,7 +14,7 @@ class PesapalServiceProvider extends ServiceProvider
 
         $this->publishes([
             __DIR__.'/config/pesapal.php' => config_path('pesapal.php'),
-            __DIR__.'/resources/views/' => resource_path('views/pesapal'),
+            __DIR__.'/resources/views/' => resource_path('views/vendor/laravel_pesapal'),
             __DIR__.'/Http/Controllers' => app_path('/Http/Controllers'),
             __DIR__.'/Models' => app_path('/'),
             __DIR__.'/database/migrations' => database_path('migrations'),
