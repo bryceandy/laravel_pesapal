9c760476a4f0bb6ba72e9a0a02c5ff1cccbdfdd2
bryceandy <bryceandy@rocketmail.com>
2020-06-15 16:16:52 +0300

    Unpublish views

--- src/PesapalServiceProvider.php
+++ src/PesapalServiceProvider.php
@@ -43,7 +43,7 @@ class PesapalServiceProvider extends ServiceProvider
     {
         $this->publishes([
             __DIR__.'/config/pesapal.php' => config_path('pesapal.php'),
-            __DIR__.'/resources/views/' => resource_path('views/vendor/laravel_pesapal'),
+            //__DIR__.'/resources/views/' => resource_path('views/vendor/laravel_pesapal'),
             __DIR__.'/assets' => public_path('pesapal'),
         ]);
     }
