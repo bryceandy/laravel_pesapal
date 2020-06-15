93f55f3c7f63ec4e36b19cd736969926465c1d3b
bryceandy <bryceandy@rocketmail.com>
2020-06-15 21:05:46 +0300

    Remove callback route

--- src/routes/web.php
+++ src/routes/web.php
@@ -5,6 +5,4 @@ use Illuminate\Support\Facades\Route;
 Route::group(['namespace' => 'Bryceandy\Laravel_Pesapal\Http\Controllers'], function(){
 
     Route::post('pesapal/iframe', 'PaymentController@store');
-
-    Route::get('callback', 'PaymentController@callback');
 });
