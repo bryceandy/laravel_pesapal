d798048bf8f49a2f7c0030949f49ff9d36530498
bryceandy <bryceandy@rocketmail.com>
2020-06-15 19:36:55 +0300

    Rename variables

--- src/Pesapal.php
+++ src/Pesapal.php
@@ -8,15 +8,15 @@ use Bryceandy\Laravel_Pesapal\OAuth\OAuthSignatureMethod_HMAC_SHA1;
 
 class Pesapal
 {
-    protected string $consumer_key;
+    protected string $consumerKey;
 
-    protected string $consumer_secret;
+    protected string $consumerSecret;
 
-    protected OAuthSignatureMethod_HMAC_SHA1 $signature_method;
+    protected OAuthSignatureMethod_HMAC_SHA1 $signatureMethod;
 
-    protected string $iframe_link;
+    protected string $iframeLink;
 
-    protected string $callback_url;
+    protected string $callbackUrl;
 
     /**
      * Pesapal constructor.
@@ -25,18 +25,18 @@ class Pesapal
      */
     public function __construct(OAuthSignatureMethod_HMAC_SHA1 $signature)
     {
-        $this->consumer_key = config('laravel_pesapal.consumer_key');
-        $this->consumer_secret = config('laravel_pesapal.consumer_secret');
-        $this->signature_method = $signature;
-        $this->iframe_link = config('laravel_pesapal.is_live') ?
+        $this->consumerKey = config('laravel_pesapal.consumer_key');
+        $this->consumerSecret = config('laravel_pesapal.consumer_secret');
+        $this->signatureMethod = $signature;
+        $this->iframeLink = config('laravel_pesapal.is_live') ?
             'https://www.pesapal.com/api/PostPesapalDirectOrderV4' :
             'https://demo.pesapal.com/api/PostPesapalDirectOrderV4';
-        $this->callback_url = config('laravel_pesapal.callback_url');
+        $this->callbackUrl = config('laravel_pesapal.callback_url');
     }
 
     /**
      * Fetches the iframe source after passing payment parameters
-     * 
+     *
      * @param $request
      * @return OAuthRequest
      */
@@ -44,17 +44,17 @@ class Pesapal
     {
         // Pesapal params
         $token = $params = NULL;
-        $post_xml = "<?xml version=\"1.0\" encoding=\"utf-8\"?><PesapalDirectOrderInfo xmlns:xsi=\"http://www.w3.org/2001/XMLSchemainstance\" xmlns:xsd=\"http://www.w3.org/2001/XMLSchema\" Currency=\"".$request->currency."\" Amount=\"".$request->amount."\" Description=\"".$request->description."\" Type=\"".$request->type."\" Reference=\"".$request->reference."\" FirstName=\"".$request->first_name?:''."\" LastName=\"".$request->last_name?:''."\" Email=\"".$request->email?:''."\" PhoneNumber=\"".$request->phone_number?:''."\" xmlns=\"http://www.pesapal.com\" />";
-        $post_xml = htmlentities($post_xml);
+        $postXml = "<?xml version=\"1.0\" encoding=\"utf-8\"?><PesapalDirectOrderInfo xmlns:xsi=\"http://www.w3.org/2001/XMLSchemainstance\" xmlns:xsd=\"http://www.w3.org/2001/XMLSchema\" Currency=\"".$request->currency."\" Amount=\"".$request->amount."\" Description=\"".$request->description."\" Type=\"".$request->type."\" Reference=\"".$request->reference."\" FirstName=\"".$request->first_name?:''."\" LastName=\"".$request->last_name?:''."\" Email=\"".$request->email?:''."\" PhoneNumber=\"".$request->phone_number?:''."\" xmlns=\"http://www.pesapal.com\" />";
+        $postXml = htmlentities($postXml);
 
-        $consumer = new OAuthConsumer($this->consumer_key, $this->consumer_secret);
+        $consumer = new OAuthConsumer($this->consumerKey, $this->consumerSecret);
 
         // Post transaction to pesapal
-        $iframe_src = OAuthRequest::from_consumer_and_token($consumer, $token, "GET", $this->iframe_link, $params);
-        $iframe_src->set_parameter("oauth_callback", $this->callback_url);
-        $iframe_src->set_parameter("pesapal_request_data", $post_xml);
-        $iframe_src->sign_request($this->signature_method, $consumer, $token);
+        $iframeSrc = OAuthRequest::from_consumer_and_token($consumer, $token, "GET", $this->iframeLink, $params);
+        $iframeSrc->set_parameter("oauth_callback", $this->callbackUrl);
+        $iframeSrc->set_parameter("pesapal_request_data", $postXml);
+        $iframeSrc->sign_request($this->signatureMethod, $consumer, $token);
         // Retrieve iframe source
-        return $iframe_src;
+        return $iframeSrc;
     }
 }
