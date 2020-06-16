d037f856574cefcb032145e54929d50c5300eb80
bryceandy <bryceandy@rocketmail.com>
2020-06-16 22:46:14 +0300

    Create a payment factory

---
+++ src/database/factories/PaymentFactory.php
@@ -0,0 +1,16 @@
+<?php
+
+use Bryceandy\Laravel_Pesapal\Payment;
+use Faker\Generator;
+use Illuminate\Support\Str;
+
+$factory->define(Payment::class, fn (Generator $faker) => [
+    'first_name' => $faker->firstName,
+    'last_name' => $faker->lastName,
+    'phone_number' => $faker->numberBetween(254000000000, 256000000000),
+    'email' => $faker->safeEmail,
+    'amount' => $faker->randomNumber(null, true),
+    'currency' => $faker->randomElement(['TZS', 'KES', 'UGX', 'USD']),
+    'reference' => Str::random(7),
+    'description' => $faker->sentence,
+]);
