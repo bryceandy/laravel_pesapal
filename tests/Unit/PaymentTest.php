788ac5a32ade250808d1a4c5326adedb36f82f16
bryceandy <bryceandy@rocketmail.com>
2020-06-17 00:14:21 +0300

    Test that a payment can be updated

--- tests/Unit/PaymentTest.php
+++ tests/Unit/PaymentTest.php
@@ -4,16 +4,62 @@ namespace Bryceandy\Laravel_Pesapal\Tests\Unit;
 
 use Bryceandy\Laravel_Pesapal\Payment;
 use Bryceandy\Laravel_Pesapal\Tests\TestCase;
+use Illuminate\Database\Eloquent\Collection;
+use Illuminate\Database\Eloquent\Model;
 use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Support\Arr;
+use Illuminate\Support\Str;
 
 class PaymentTest extends TestCase
 {
     use RefreshDatabase;
 
+    /**
+     * @var array
+     */
+    private array $transaction;
+
+    /**
+     * @var Collection|Model
+     */
+    private $payment;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $reference = Str::random(7);
+
+        $this->payment = factory(Payment::class)->create([
+            'reference' => $reference,
+        ]);
+
+        $this->transaction = [
+            'pesapal_merchant_reference' =>$reference,
+            'status' => Arr::random(['PENDING', 'COMPLETED', 'FAILED', 'CANCELLED']),
+            'payment_method' => Arr::random(['MPESA', 'VISA', 'MASTERCARD', 'TIGOPESA']),
+            'pesapal_transaction_tracking_id' => Str::random(),
+        ];
+    }
+
+    /** @test */
+    public function payments_can_be_updated()
+    {
+        $payment = $this->payment;
+
+        $payment->modify($this->transaction);
+
+        $this->assertDatabaseHas('pesapal_payments', [
+            'id' => $payment->id,
+            'status' => $this->transaction['status'],
+            'tracking_id' => $this->transaction['pesapal_transaction_tracking_id'],
+        ]);
+    }
+
     /** @test */
     public function a_payment_can_be_created_with_a_factory()
     {
-        factory(Payment::class)->create();
+        $this->payment;
 
         $this->assertCount(1, Payment::all());
     }
