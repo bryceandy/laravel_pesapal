6fc492e6887d8951299cd0f2c55fb52c0ce57806
bryceandy <bryceandy@rocketmail.com>
2020-06-16 22:52:58 +0300

    Add an example test

---
+++ tests/Unit/ExampleTest.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Bryceandy\Laravel_Pesapal\Tests\Unit;
+
+use Bryceandy\Laravel_Pesapal\Tests\TestCase;
+
+class ExampleTest extends TestCase
+{
+    /**
+     * A sample test
+     *
+     * @test
+     */
+    public function testBasicTest()
+    {
+        $this->assertTrue(true);
+    }
+}
