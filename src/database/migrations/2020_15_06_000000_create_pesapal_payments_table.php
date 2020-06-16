94e5df6ebeef0869e672f638eb0bbba6218fb9ea
bryceandy <bryceandy@rocketmail.com>
2020-06-16 16:13:53 +0300

    Rename migration class

--- src/database/migrations/2020_15_06_000000_create_pesapal_payments_table.php
+++ src/database/migrations/2020_15_06_000000_create_pesapal_payments_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreatePaymentsTable extends Migration
+class CreatePesapalPaymentsTable extends Migration
 {
     /**
      * Run the migrations.
