d8406c875502bd35b3f7a42a4bce6a9c56c45448
bryceandy <bryceandy@rocketmail.com>
2020-06-18 15:46:53 +0300

    Add route name

--- src/routes/web.php
+++ src/routes/web.php
@@ -4,5 +4,5 @@ use Illuminate\Support\Facades\Route;
 
 Route::group(['namespace' => 'Bryceandy\Laravel_Pesapal\Http\Controllers'], function(){
 
-    Route::post('pesapal/iframe', 'PaymentController@store');
+    Route::post('pesapal/iframe', 'PaymentController@store')->name('payment.store');
 });
