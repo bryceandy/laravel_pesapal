f5d15b874f3d3d00a7aa6e98aa6e7d2a67dc25dd
bryceandy <bryceandy@rocketmail.com>
2021-04-26 19:31:04 +0300

    Add custom exception message

--- src/Http/Middleware/ValidateConfigMiddleware.php
+++ src/Http/Middleware/ValidateConfigMiddleware.php
@@ -8,18 +8,24 @@ use Closure;
 class ValidateConfigMiddleware
 {
     /**
-     * Ensures pesapal configurations are always available
+     * Ensures Pesapal configurations are always available
+     *
      * @param $request
      * @param Closure $next
+     *
      * @return mixed
+     *
      * @throws ConfigurationUnavailableException
      */
     public function handle($request, Closure $next)
     {
-        if (!config('pesapal.consumer_key') ||
-            !config('pesapal.consumer_secret') ||
-            !config('pesapal.callback_url'))
-            throw new ConfigurationUnavailableException();
+        if (!config('pesapal.consumer_key')
+            || !config('pesapal.consumer_secret')
+            || !config('pesapal.callback_url')
+        )
+            throw new ConfigurationUnavailableException(
+                'Your configuration values are missing. Add your consumer key, secret and callback URL for Pesapal'
+            );
 
         return $next($request);
     }
